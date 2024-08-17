b3fa0aba5c6bc433a293b969373b63318d8743be
Paula Miniño. <pato_m@outlook.com>
2024-08-17 14:56:24 -0300

    Solución click

--- eventos.js
+++ eventos.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () { 
 const imDiv = document.querySelector("#soydiv")
-function alerta () {
+
+function alerta (event) {
 alert ("¡Hola, soy el Div!")
+event.stopPropagation();
+
 }
 
 imDiv.addEventListener('click', alerta);
-});
+
+
+const button = document.getElementById("btn");
+function botonalerta (event2){
+    alert("¡Hola!")
+    event2.stopPropagation();
+}
+button.addEventListener('click', botonalerta);
+
+
+}); 
+
